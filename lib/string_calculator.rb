ac2f16820bdbe42580c01c90e7e5b482a637877a
Shivangi140394 <shivangi14shrivastava@gmail.com>
2024-03-09 06:31:25 +0530

    add the string calculator method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,22 @@
 class StringCalculator
-  # Add logic here
+  def add(numbers)
+    return 0 if numbers.empty?
+
+    delimiter = ","
+    if numbers.start_with?("//")
+      custom_delimiters = numbers.scan(/\[(.*?)\]/).flatten.map { |d| Regexp.escape(d) }
+      delimiter = Regexp.new(custom_delimiters.join("|"))
+      numbers = numbers.split("\n", 2).last
+    end
+
+    numbers = numbers.gsub(/[\n]/, delimiter == "," ? "," : "")
+
+    nums = numbers.split(delimiter).map(&:to_i).reject { |num| num > 1000 }
+    negatives = nums.select { |num| num < 0 }
+    if negatives.any?
+      raise "negative numbers not allowed #{negatives.join(',')}"
+    end
+
+    nums.sum
+  end
 end
